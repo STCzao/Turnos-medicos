298953a618168f6c0bbf7b908d24e1b0267520f5
STCzao <Santicoronel0183@gmail.com>
2025-06-13 02:45:13 -0300

    Empezamos a trabajar con los componentes dentro del Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Home from './components/Home'
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <div>
       <Home/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
