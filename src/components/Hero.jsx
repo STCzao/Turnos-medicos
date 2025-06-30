e22db573d958aa515a080b135b9078bd00836070
STCzao <Santicoronel0183@gmail.com>
2025-06-30 06:15:33 -0300

    Comentamos las funcionalidades de cada porcion de codigo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,23 @@ const Hero = () => {
         t.medico === nuevoTurno.medico
     );
 
+    {/* En este bloque tenemos el objeto nuevoTurno como parametro. La funcion
+      callback nos devolvera una constante llamada duplicado que recibira
+      los valores del array almacenado en el estado turnos. Iteramos con el metodo
+      some. t hace referencia a cada valor del array; i hace referencia a su 
+      posicion en el array.
+      Consultamos si el nuevo valor no coincide con el valor guardado en el index 
+      del array almacenado en turnoeditando; es decir, no esta en posicion para ser editado. 
+      Ahora comparamos el turno guardado con el que sera ingresado mediante el
+      nuevo objeto , tanto para el medico como la fecha; tienen que cumplirse las tres condiciones */}
+
     if (duplicado) {
       alert("Ya existe un turno para ese médico en esta fecha");
       return;
     }
+
+    {/* Si los valores almacenados anteriormente en duplicado son repetidos,
+      recibimos un alert informando la existencia de algun turno previo */}
 
     if (turnoEditando !== null) {
       const actualizados = [...turnos];
@@ -30,7 +43,18 @@ const Hero = () => {
     } else {
       setTurnos([...turnos, nuevoTurno]);
     }
+
+    {/* Consultamos si el estado de turnoeditando entro en actualizacion. En este
+      caso es true. Por eso guardaremos las actualizaciones en una constante que
+      realizara una copia del array de turnos con el spread operator. Accedemos
+      al array de actualizados en el lugar indicado por turnoeditando; en este caso
+      es el index y el asignamos el nuevo turno. El array de turnos es reemplazado
+      por el de actualizados con el nuevo turno en su nueva posicion; reseteamos 
+      el estado de turnoeditando a null.
+      Si turnoeditando no contiene valores, hacemos una copia del array de turnos
+      y el nuevoturno es añadido al final del array */}
   };
+
 
   const handleEliminarClick = (index) => {
     setTurnoAEliminarIndex(index);
@@ -43,7 +67,6 @@ const Hero = () => {
       setTurnos(actualizados);
       if (turnoEditando?.index === turnoAEliminarIndex) setTurnoEditando(null);
     }
-
     setMostrarConfirmacion(false);
     setTurnoAEliminarIndex(null);
   };
@@ -83,7 +106,7 @@ const Hero = () => {
           <div className="bg-white divide-gray-300/60 flex space-x-5 divide-x pl-3 text-sm rounded border border-gray-300/60 shadow-lg">
             <div className="py-2.5 flex justify-center flex-col">
               <h3 className="text-gray-700 font-medium">
-                Confirmar Eliminación
+                Confirmar eliminación
               </h3>
               <p className="text-gray-500 max-w-64">
                 ¿Estás seguro de que quieres eliminar este turno? Esta acción no
